a7c4d17a1cacfa360268bac50fe1b89feed6474f
johana <johan.alm@who-umc.org>
2023-12-07 23:19:36 +0100

    Day3, Puzzle2

--- Day3.fs
+++ Day3.fs
@@ -40,7 +40,7 @@ module IndexSpan =
     for i = 0 to span.Length + 1 do
       result.Add { u with Column = x.Column + i }
       result.Add { v with Column = x.Column + i }
-    result
+    result :> seq<Index>
 
 
 [<RequireQualifiedAccess>]
@@ -73,28 +73,73 @@ let isSymbol (schm : Schematic) (index : Index) =
   index.Column >= 0 && index.Column < schm.[index.Row].Length &&
   Char.isSymbol schm[index.Row].[index.Column]
 
-let isSymbolAdjacent (schm : Schematic) (span : IndexSpan) =
-  span |> IndexSpan.adjacents |> Seq.exists (isSymbol schm)
+let isSymbolAdjacent (schm : Schematic) =
+  IndexSpan.adjacents >> Seq.exists (isSymbol schm)
 
-let accumulateNumberSpans (schm : Schematic) (result : ResizeArray<IndexSpan>) =
+let getNumberSpans (schm : Schematic) =
+  let result = ResizeArray<IndexSpan> ()
   for r = 0 to schm.Length - 1 do
     let mutable c = 0
     while c < schm[r].Length do
       match numberSpan schm { Row = r; Column = c } with
       | Some span  -> result.Add span; c <- span.Start.Column + span.Length
       | None -> c <- c + 1
+  result
 
 let getSchematic (input : string) =
   input
   |> File.ReadAllLines
   |> Array.map (fun s -> s.ToCharArray ())
 
+
 module Puzzle1 =
+
   let solve (input : string) =
     let schm = getSchematic input
-    let numberSpans = ResizeArray<IndexSpan> ()
-    accumulateNumberSpans schm numberSpans
-    numberSpans
+    schm
+    |> getNumberSpans
     |> Seq.filter (isSymbolAdjacent schm)
     |> Seq.sumBy (numberSpanToInt schm)
 
+
+module Puzzle2 =
+
+
+  type AdjacencyMap = Map<Index, IndexSpan list>
+
+  [<RequireQualifiedAccess>]
+  module AdjacencyMap =
+
+    let add (map : AdjacencyMap) (span : IndexSpan) =
+      Seq.fold
+        (fun m i ->
+          match Map.tryFind i m with
+          | Some spans -> Map.add i (span :: spans) m
+          | None -> Map.add i [ span ] m)
+        map (IndexSpan.adjacents span)
+
+
+  let mkAdjacencyMap (schm : Schematic) =
+    Seq.fold AdjacencyMap.add Map.empty (getNumberSpans schm)
+  
+  let getGears (schm : Schematic) (map : AdjacencyMap) (index : Index) =
+    match schm[index.Row][index.Column] with
+    | c when c = '*' ->
+      match Map.tryFind index map with
+      | Some ([ a ; b] as gears) -> Some gears
+      | _ -> None
+    | _ -> None
+  
+  let gearRatio schm =
+    List.fold (fun acc span -> acc * numberSpanToInt schm span) 1
+  
+  let getIndices =
+    Seq.mapi (fun r row -> Seq.mapi (fun c _ -> { Row = r; Column = c }) row)
+    >> Seq.concat
+
+  let solve (input : string) =
+    let schm = getSchematic input
+    let map = mkAdjacencyMap schm
+    getIndices schm
+    |> Seq.choose (getGears schm map)
+    |> Seq.sumBy (gearRatio schm) 
